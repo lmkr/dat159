1e3ee362990878f356a3dfdd3830e7b0a0812f71
Lars Michael Kristensen <lmkr@hvl.no>
2018-10-24 22:19:45 +0200

    input output first version

--- iotdevices/arduino/lecture1/InputOutput/InputOutput.ino
+++ iotdevices/arduino/lecture1/InputOutput/InputOutput.ino
@@ -1,9 +1,55 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(12, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+int active = 0;
+int changed = 0;
+byte brightness = 0;
+
+void loop()
+{
+  int btn = digitalRead(12);
+
+  if ((btn == HIGH) && (!changed)) {
+    changed = 1;
+    active = 1 - active;
+    if (active)
+      digitalWrite(13, HIGH);
+    else {
+      digitalWrite(13, LOW);
+      analogWrite(9, 0);
+    }
+  }
+
+  if (btn == LOW)
+    changed = 0;
+
+  int potval = analogRead(A0);
+  int photoval = analogRead(A1);
+
+  Serial.print(photoval);
+  Serial.print("<");
+  Serial.print(potval);
+  Serial.print(":");
+
+  if ((photoval < potval) && active) {
+
+    brightness = min(potval - photoval, 255);
+    analogWrite(9, brightness);
+    Serial.print(brightness);
+
+  } else {
+
+    brightness = 0;
+    analogWrite(9, 0);
+
+  }
+
+  Serial.println();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
