9b0ab2c53522f2df8d91a2ec3d230a5665cf4ad2
Tosin Daniel Oyetoyan <tosindo@idi.ntnu.no>
2018-11-01 10:44:49 +0100

    week 2 - Hands-on-assignment

---
+++ mqtt-cloud/MQTTPubTemperature.java
@@ -0,0 +1,77 @@
+package publish;
+
+import java.util.Random;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/*
+ * tosindo
+ */
+
+public class MQTTPubTemperature {
+	
+	private String topic = "Temp";
+    private int qos = 1;
+    private String broker = "tcp://m23.cloudmqtt.com:14331";
+    private String clientId = "MQTT_Temperature";
+    private String username = "bstsnwbg";
+    private String password = "r9KiZDnb0_cF";
+    
+    private MqttClient publisherClient;
+    private Random rand;
+    
+	public MQTTPubTemperature() {
+		
+		rand = new Random();
+	}
+	
+	private void publish() throws MqttPersistenceException, MqttException, InterruptedException {
+		
+		//loop forever
+		while(true) {
+			String temp = String.valueOf(rand.nextInt(60));
+	    	System.out.println("Publishing message: "+temp);
+	        MqttMessage message = new MqttMessage(temp.getBytes());
+	        message.setQos(qos);
+	        publisherClient.publish(topic, message);
+	        Thread.sleep(4000);
+		}
+		
+	}
+	
+	private void connect() {
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+        	publisherClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setUserName(username);
+            connOpts.setPassword(password.toCharArray());
+            System.out.println("Connecting to broker: "+broker);
+            publisherClient.connect(connOpts);
+            System.out.println("Connected");
+            
+        } catch(MqttException e) {
+            System.out.println("reason "+e.getReasonCode());
+            System.out.println("msg "+e.getMessage());
+            System.out.println("loc "+e.getLocalizedMessage());
+            System.out.println("cause "+e.getCause());
+            System.out.println("excep "+e);
+            e.printStackTrace();
+        }
+	}
+	
+	public static void main(String[] args) throws InterruptedException, MqttPersistenceException, MqttException {
+        
+		MQTTPubTemperature mqttpub = new MQTTPubTemperature();
+		mqttpub.connect();
+		mqttpub.publish();
+		
+    }
+}
---
+++ mqtt-cloud/MQTTSubTemperature.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package subscribe;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/**
+ * @author tosindo
+ *
+ */
+public class MQTTSubTemperature implements MqttCallback{
+	
+	private String message;
+	
+	public MQTTSubTemperature() throws MqttException {
+		String topic    = "Temp";
+        int qos         = 1;		//1 - This client will acknowledge to the Device Gateway that messages are received
+        String broker   = "tcp://m23.cloudmqtt.com:14331";
+        String clientId = "MQTT_Temperature_SUB";
+        String username = "bstsnwbg";
+        String password = "r9KiZDnb0_cF";
+    	
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(true);
+        connOpts.setUserName(username);
+        connOpts.setPassword(password.toCharArray());
+        
+        System.out.println("Connecting to broker: "+broker);
+        
+        MqttClient client = new MqttClient(broker, clientId, new MemoryPersistence());
+        client.setCallback(this);
+        client.connect(connOpts);
+        System.out.println("Connected");
+        
+        client.subscribe(topic, qos);
+        System.out.println("Subscribed to message");
+
+	}
+    
+    /**
+     * @see MqttCallback#connectionLost(Throwable)
+     */
+	public void connectionLost(Throwable cause) {
+		System.out.println("Connection lost because: " + cause);
+		System.exit(1);
+		
+	}
+	
+	/**
+	 * @see MqttCallback#messageArrived(String, MqttMessage)
+	 */
+	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		System.out.println(String.format("[%s] %s", topic, new String(message.getPayload())));
+		this.setMessage(new String(message.getPayload()));
+	}
+	
+	/**
+	 * @see MqttCallback#deliveryComplete(IMqttDeliveryToken)
+	 */
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+    public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public static void main(String args[]) throws MqttException {
+    	new MQTTSubTemperature();
+    	
+    }
+
+}
