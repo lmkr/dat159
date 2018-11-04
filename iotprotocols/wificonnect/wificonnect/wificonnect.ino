fa538cbc14c37063c44aa6662e0160531a71564c
Lars Michael Kristensen <lmkr@hvl.no>
2018-11-04 19:27:09 +0100

    revised wifi sketches

--- iotprotocols/wificonnect/wificonnect/wificonnect.ino
+++ iotprotocols/wificonnect/wificonnect/wificonnect.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 
 char ssid[] = "os21-1-netgear";     //  your network SSID (name) 
-char pass[] = "XXXXX";    // your network password
+char pass[] = "cpnets89";    // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 void setup() {
@@ -20,10 +20,48 @@ void setup() {
   // if you are connected, print out info about the connection:
   else {
     Serial.println("Connected to network");
+    printWifiStatus();
   }
 }
 
 void loop() {
   // do nothing
 }
- 
+
+void printWifiStatus() {
+  
+  // print information about the wifi connection
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  printMacAddress();
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void printMacAddress() {
+
+  byte mac[6];                     
+
+  // print your MAC address:
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+}
