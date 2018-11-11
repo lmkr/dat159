a026ff9a7c34ed16edc3a847bf2bf8da91fe4853
Lars Michael Kristensen <lmkr@hvl.no>
2018-11-11 14:08:47 +0100

    added routes for sample application

--- cloud/sparkjava/src/main/java/no/hvl/dat159/Main.java
+++ cloud/sparkjava/src/main/java/no/hvl/dat159/Main.java
@@ -1,11 +1,41 @@
 package no.hvl.dat159;
 
 import static spark.Spark.*;
+import com.google.gson.*;
 
 public class Main {
+	
+	static Temperature temp = null;
+	
     public static void main(String[] args) {
     	
+    	temp = new Temperature();
+    	
     	port(8080);
-        get("/hello", (req, res) -> "Hello World");
+    	
+    	after((req, res) -> {
+    		  res.type("application/json");
+    		});
+    	
+        get("/tempsensor/current", (req, res) -> tempToJson());
+        
+        put("/tempsensor/current", (req,res) -> {
+        
+        	Gson gson = new Gson();
+        	
+        	temp = gson.fromJson(req.body(), Temperature.class);
+        
+            return tempToJson();
+        	
+        });
+    }
+    
+    static String tempToJson () {
+    	
+    	Gson gson = new Gson();
+    	    
+    	String jsonInString = gson.toJson(temp);
+    	
+    	return jsonInString;
     }
 }
---
+++ cloud/sparkjava/src/main/java/no/hvl/dat159/Temperature.java
@@ -0,0 +1,20 @@
+package no.hvl.dat159;
+
+public class Temperature {
+
+	double temperature;
+
+	public Temperature () {
+		this.temperature = 0.0;
+	}
+	
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	
+	
+}
